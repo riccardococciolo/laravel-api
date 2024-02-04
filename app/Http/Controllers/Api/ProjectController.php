1487a4a79f56285f7b0ed60690c94698468e5e36
riccardococciolo <73590570+riccardococciolo@users.noreply.github.com>
2024-02-04 12:21:25 +0100

    add: details route

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,9 +3,35 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+        $projects = Project::all();
+
+        return response()->json([
+            'results' => $projects,
+        ]);
+    }
+
+
+    public function show(string $slug)
+    {
+        $project = Project::with('type')->where('slug', $slug)->first();
+
+        if ($project) {
+            return response()->json([
+                'results' => $project,
+                'success' => true
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Nessun post trovato'
+            ]);
+        }
+    }
 }
